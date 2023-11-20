95c86df49f0b50e72b3c0889115ae89a30d3ba8b
Julie Van Houdenhove <100696436+JulieVanHoudenhove@users.noreply.github.com>
2023-11-20 15:50:16 +0100

    feat: create and add component button

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { useState } from 'react';
+import CustomButton from './Components/Button';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+    const [text, setText] = useState("rien");
+
+    const changeText = (newText) => {
+        setText(newText);
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text>j'ai cliqué sur {text}</Text>
+            <CustomButton
+                text={'noir'}
+                changeText={changeText}
+                color={'#000000'}
+            />
+            <CustomButton
+                text={'bleu'}
+                changeText={changeText}
+                color={'blue'}
+            />
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
